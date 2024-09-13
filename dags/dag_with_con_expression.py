278701734f1cb6438c1841fb1b9cb29b99e7596a
abhay8046 <abhaydere8046@gmail.com>
2024-09-13 12:13:33 -0600

    Updated retry delay for con_expression dag

--- dags/dag_with_con_expression.py
+++ dags/dag_with_con_expression.py
@@ -5,7 +5,7 @@ from airflow.operators.python import PythonOperator
 default_args={
     'owner':'Abhay',
     'retries':2,
-    'retry_delay':timedelta(minutes=2)
+    'retry_delay':timedelta(minutes=5)
 }
 
 def prints(cmd):
